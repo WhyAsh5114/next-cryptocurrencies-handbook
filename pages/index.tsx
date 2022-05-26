20114898371d31bff615a20a6f57148922b7a6ea
WhyAsh5114 <whyash5114@gmail.com>
2022-05-27 00:43:12 +0530

    tailwind setup

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,23 @@
+import { Center, Title, TextInput } from "@mantine/core";
+
 export default function Home() {
     return (<>
-        <h1>Home Page Boi</h1>
+        <Center>
+            <Title
+                order={1}
+                style={{ paddingTop: 25 }}
+            >
+                Token Viewer App
+            </Title>
+        </Center>
+
+        <div>
+            <TextInput
+                placeholder="USDC"
+                label="Search Token"
+                radius="xl"
+                size="xl"
+            />
+        </div>
     </>);
 }
