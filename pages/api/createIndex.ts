3f02d66b3c5d5542e64df8ac37eebd7903f2058d
WhyAsh5114 <whyash5114@gmail.com>
2022-05-27 17:36:11 +0530

    added a createIndex endpoint to update index

---
+++ pages/api/createIndex.ts
@@ -0,0 +1,7 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { createIndex } from "../../lib/redis";
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+    await createIndex();
+    res.status(200).send("ok");
+}
