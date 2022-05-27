06f61d33c80820b7a98ccfad1619b8e5c9bc17a0
WhyAsh5114 <whyash5114@gmail.com>
2022-05-27 17:35:56 +0530

    created a different component for search

---
+++ lib/SearchForm.tsx
@@ -0,0 +1,51 @@
+import { useState } from "react";
+import { Image, List, TextInput } from "@mantine/core";
+import { Search } from "tabler-icons-react";
+import { Entity } from "redis-om";
+
+class Token extends Entity {}
+interface Token {
+    name: string;
+    abbreviation: string;
+    token_address: string;
+    logo: string;
+    description: string;
+}
+
+export default function TokenForm () {
+    const [hits, setHits] = useState<Token[]>([]);
+
+    const search = async (event: any) => {
+        const q = event.target.value;
+        console.log(q);
+        if (q.length > 2) {
+            const params = new URLSearchParams({ q });
+            const res = await fetch('/api/search?' + params);
+            const result = await res.json();
+            console.log(result);
+            setHits(result.tokens);
+        }
+    }
+
+    return (
+        <div>
+            <TextInput
+                label="Search token"
+                radius="sm"
+                size="lg"
+                type="search"
+                icon={<Search size={25}/>}
+                className="w-3/4 md:w-1/2 lg:w-1/3"
+                onChange={search}
+            />
+
+            <List>
+                {hits.length > 0 ? hits.map((hit: Token) => (
+                    <List.Item key={hit.entityId}>
+                        <Image src={hit.logo} width={100} alt={`${hit.abbreviation} logo`}></Image>
+                    </List.Item>
+                )) : <></>}
+            </List>
+        </div>
+    );
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,12 @@
-import { Title, TextInput, Text } from "@mantine/core";
-import { Search } from "tabler-icons-react";
+import { Title, Text, Image, Card } from "@mantine/core";
+import TokenForm from "../lib/SearchForm";
 
 export default function Home() {
     return (<>
         <div className="grid place-items-center h-full my-10 gap-y-12">
             <Title
                 order={1}
-                style={{ paddingTop: 25 }}
+                style={{ paddingTop: 25, color: "white" }}
             >
                 Token Viewer App
             </Title>
@@ -21,14 +21,7 @@ export default function Home() {
                 Get token info on various blockchains from one app
             </Text>
 
-            <TextInput
-                label="Search token"
-                radius="sm"
-                size="lg"
-                type="search"
-                icon={<Search size={25}/>}
-                className="w-3/4 md:w-1/2 lg:w-1/3"
-            />
+            <TokenForm />
         </div>
     </>);
 }
