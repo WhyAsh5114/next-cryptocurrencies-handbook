cbccd47b9220d9da25938f2ff8076d8b0a0515e3
WhyAsh5114 <whyash5114@gmail.com>
2022-05-28 22:19:34 +0530

    added some definitions and made search more consistent by matching the query and the coin properties

--- lib/database.ts
+++ lib/database.ts
@@ -24,63 +24,63 @@ const database = {
         "GUSD",
         "0x056Fd409E1d7A124BD7017459dFEa2F387b6d5Cd",
         "https://cryptologos.cc/logos/gemini-dollar-gusd-logo.png?v=022",
-        "",
+        "A stablecoin offering from Gemini (a cryptocurrency exchange), which is backed by normal USD in a 1:1 ratio, its supported on many exchanges and Dapps, available on the Ethereum blockchain. According to their website, GUSD is transparently audited, regulated, insured and secure. It has zero fees when buying or selling with normal USD on the Gemini Exchange.",
         ["Stablecoin", "Multi-chain DeFi"],
-        ""
+        "https://www.gemini.com/dollar/"
     ),
     DAI: new Token(
         "Dai Stablecoin",
         "DAI",
         "0x6B175474E89094C44Da98b954EedeAC495271d0F",
         "https://cryptologos.cc/logos/multi-collateral-dai-dai-logo.png?v=022",
-        "",
+        "DAI is a stablecoin offering from MakerDAO, it's commonly used on all Dapps and exchanges. Unlike traditional stablecoins backed by USD in a 1:1 ratio, DAI is backed by regular cryptocurrencies like Ethereum, to account for the volatility in prices of crypto, it is over-collaterised, which helps it maintain its 1:1 peg with the US Dollar, because its backed by a DAO and not a company, it is supposed to be more decentralized.",
         ["Stablecoin", "Multi-chain DeFi"],
-        ""
+        "https://makerdao.com/en/"
     ),
     BUSD: new Token(
         "Binance USD",
         "BUSD",
         "0x4Fabb145d64652a948d72533023f6E7A623C7C53",
         "https://cryptologos.cc/logos/binance-usd-busd-logo.png?v=022",
-        "",
+        "BUSD is a stablecoin offering from Binance, one of the biggest and well known cryptocurrency exchanges, this token is extremely prevalent on the Binance Smart Chain (BSC) and is pegged to the US Dollar in a 1:1 ratio, backed by normal (fiat) USD. According to their website it's fully regulated.",
         ["Stablecoin", "Multi-chain DeFi"],
-        ""
+        "https://www.binance.us/en/busd/"
     ),
     USDT: new Token(
         "USD Tether",
         "USDT",
         "0xdAC17F958D2ee523a2206206994597C13D831ec7",
         "https://cryptologos.cc/logos/tether-usdt-logo.png?v=022",
-        "",
+        "One of the most commonly used stablecoins spanning across multiple blockchains is USDT, its parent company is Tether. According to the website, USDT is pegged to the US Dollar in a 1:1 ratio and fully backed and transparent, but there have been multiple instances of Tether being uncomfortable to share its reserves in the past and is not considered safe by some crypto enthusiasts. Despite all that, it is the most commonly used (for now), and also prevalent in multiple blockchains.",
         ["Stablecoin", "Multi-chain DeFi"],
-        ""
+        "https://tether.to/en/"
     ),
     USDC: new Token(
         "USD Coinbase",
         "USDC",
         "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48",
         "https://cryptologos.cc/logos/usd-coin-usdc-logo.png?v=022",
-        "",
+        "USDC is a stablecoin from Coinbase. You can always redeem 1 USD Coin for US$1.00, giving it a stable price. On Coinbase, eligible customers can earn rewards for every USD Coin they hold. USDC is one of the most commonly seen stablecoins on different blockchains, it is also considered more stable and secure by crypto enthusiasts and is 100% backed according to coinbase.",
         ["Stablecoin", "Multi-chain DeFi"],
-        ""
+        "https://www.coinbase.com/usdc/"
     ),
     ETH: new Token(
         "Ethereum",
         "ETH",
         "",
         "https://cryptologos.cc/logos/ethereum-eth-logo.png?v=022",
-        "",
+        "Ethereum is the community-run technology powering the cryptocurrency ether (ETH) and thousands of decentralized applications. The 2nd-generation blockchain which supports smart contracts. Smart contracts allow us to deal with tokens and transact using programming. This opens up lots of different opportunities, like decentralized finance, programmable trustless smart contracts.",
         ["Blockchain", "DeFi"],
-        ""
+        "https://ethereum.org/"
     ),
     SOL: new Token(
         "Solana",
         "SOL",
-        "",
+        "Solana is a decentralized blockchain built to enable scalable, user-friendly apps for the world. It's also referred to as the 3rd generation of blockchain as its extremely fast and even cheaper than Polygon MATIC, its based on the consensus mechanism of PoH (Proof of History) which is faster and cheaper than PoW (Proof of Work). Solana also has its drawbacks, its not as decentralized as something like Ethereum and has had some shutdowns in the past. It's lack of nodes is... concerning to say the least.",
         "https://cryptologos.cc/logos/solana-sol-logo.png?v=022",
         "",
         ["Blockchain", "DeFi"],
-        ""
+        "https://solana.com/"
     ),
     AVAX: new Token(
         "Avalanche",
@@ -107,7 +107,7 @@ const database = {
         "https://cryptologos.cc/logos/polygon-matic-logo.png?v=022",
         "Polygon was originally a layer 2 solution to Ethereum, i.e. it was an alternate blockchain where we could perform many transactions with low gas fees, but eventually it grew an ecosystem of Dapps on its own and is now filled with tons of opportunities. Transactions on Polygon have a fee of less than a dollar, therefore we can move our funds around as much as we need without losing money to gas fees. Many big Dapps and DAOs have released a Polygon platform like Curve, Aave and Balancer.",
         ["Blockchain", "DeFi", "Ethereum Layer 2"],
-        "https://polygon.technology/"
+        "https://polygon.technology"
     ),
     BTC: new Token(
         "Bitcoin",
@@ -132,12 +132,20 @@ const database = {
 export default function search(query: string) {
     let matched_coins: Array<Token> = [];
     for (let [index, coin] of Object.entries(database)) {
-        for (let [property, property_value] of Object.entries(coin)) {
-            if (property_value.includes(query)) {
-                matched_coins.push(coin);
-                break;
-            }
+        if (coin.abbreviation.toLowerCase().includes(query.toLowerCase()) || coin.name.toLowerCase().includes(query.toLowerCase())) {
+            matched_coins.push(coin);
         }
     }
     return matched_coins;
+}
+
+export function match(query: string) {
+    let matched_coin: Token = new Token("", "", "", "", "", [""], "");
+    for (let [index, coin] of Object.entries(database)) {
+        if (coin.name === (query)) {
+            matched_coin = coin;
+            break;
+        }
+    }
+    return matched_coin;
 }
