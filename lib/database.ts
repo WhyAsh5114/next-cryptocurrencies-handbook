1440d4c014f0d1b1fe8c9e11bf0be7a3d30d3c92
WhyAsh5114 <whyash5114@gmail.com>
2022-05-29 00:52:54 +0530

    fixed dogecoin broken link

--- lib/database.ts
+++ lib/database.ts
@@ -56,10 +56,10 @@ const database = {
         "https://tether.to/en/"
     ),
     DOGE: new Token(
-        "DogeCoin",
+        "Dogecoin",
         "DOGE",
         "",
-        "https://images.app.goo.gl/WkY9UZ453TCYosfb8",
+        "https://cryptologos.cc/logos/dogecoin-doge-logo.png?v=022",
         "Dogecoin is a cryptocurrency created by software engineers Billy Markus and Jackson Palmer, who decided to create a payment system as a joke, making fun of the wild speculation in cryptocurrencies at the time. It is considered both the first meme coin, and, more specifically, the first dog coin.",
         ["Memecoin"],
         "https://dogecoin.com/"
